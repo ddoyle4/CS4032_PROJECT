a49cdb7defa59897dc5d65ad9d276c9f2a31ca63
David Doyle <ddoyle3@tcd.ie>
2016-11-29 22:59:01 +0000

    Adding 'etc' file handling capabilities to client to persist information between calls

--- client/src/Lib.hs
+++ client/src/Lib.hs
@@ -4,31 +4,52 @@ module Lib  where
 import System.Environment
 import Control.Monad.IO.Class
 import System.Directory
+import Control.Monad
+import Data.List.Split
 
 
-host :: String
-host = "http://localhost"
+data Server = AuthServer | FileServer 
 
-port :: String
-port = "8081"
+instance Show Server where
+  show FileServer = "The File Server"
+  show AuthServer = "The Authentification Server"
 
-{-
-saveEntry :: [String] -> IO ()
-saveEntry params = liftIO $ do
-  c <- openConnection host port
-  let q = buildRequest1 $ do
-		http GET "/"
-		setAccept "text/html"
 
-  sendRequest c q emptyBody
-	receiveResponse c (\p i -> do
-		xm <- Streams.read i
-		case xm of
-				Just x    -> S.putStr x
-				Nothing   -> "")
+--reads connection info from an ETC Connection Info File
+readEtcConnectionFile :: Server -> IO [String]
+readEtcConnectionFile s = do
+  d <- etcDir
+  let pathEtc = d ++ etcFileName
+  ensureEtcExists s pathEtc
+  fileContents <- readFile pathEtc
+  return (splitOn "\n" fileContents)
+  where
+  etcFileName = case s of
+    AuthServer -> authServerEtc
+    FileServer -> fileServerEtc
 
-	closeConnection c
--}
+--retrieves host:port info save on file, or requests from user if absent
+getConnectionInfo :: Server -> IO ConnectionInfo  
+getConnectionInfo s = do
+  details <- readEtcConnectionFile s
+  return (ConnectionInfo (details !! 0) (details !! 1))
+
+
+
+etcFilename :: String
+etcFilename = "/.haskell_client"
+
+authServerEtc :: String
+authServerEtc = "auth_server_ci"
+
+fileServerEtc :: String
+fileServerEtc = "file_server_ci"
+
+data ConnectionInfo = ConnectionInfo 
+  { hostAddr :: String
+  , hostPort :: String
+  } deriving Show
+
 
 helloWorld :: IO ()
 helloWorld = liftIO $ do
@@ -43,20 +64,29 @@ processArgs [] = liftIO $ do
   putStrLn "You didn't provide any args"
 
 
+etcDir :: IO String
+etcDir = do
+  h <- getHomeDirectory 
+  return (h ++ etcFilename)
+
+
 ensureHomeDir :: IO ()
-ensureHomeDir = liftIO $ do
-	homeDir <- getHomeDirectory
-	exists <- doesDirectoryExist (homeDir ++ ".haskell_client") 
-	if not exists
-	then return () 
-    --createDirectory homeDir ++ ".haskell_client"
-    --return ()
-	else return ()
-	
+ensureHomeDir = do
+  d <- etcDir
+  ex <- doesDirectoryExist d
+  when (not ex) (do 
+    putStrLn ("Creating new haskell client directory:" ++ d)
+    createDirectory d)
+
+
+
 someFunc :: IO ()
 someFunc = do
   args <- getArgs
+  ensureHomeDir
+
   processArgs args
+
 
 
 
